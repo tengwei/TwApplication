def288f6c9f5f4e6051a20f55dd270368a6387cb
papawei <tengweichen@126.com>
2015-10-07 19:52:58 +0800

    123

---
+++ AspNetMvcVsWebApiTest/AspNetMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMvc.Startup))]
+namespace AspNetMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
