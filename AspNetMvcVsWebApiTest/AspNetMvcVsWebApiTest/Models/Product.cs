81cf64cd7817b49872416c73d7cbf6b2dad0d60e
papawei <tengweichen@126.com>
2015-10-07 23:22:33 +0800

    fgyjfg

---
+++ AspNetMvcVsWebApiTest/AspNetMvcVsWebApiTest/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ProductsApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Json {
+        public object Products1 { get; set; }
+        public object Products2 { get; set; }
+    }
+}
