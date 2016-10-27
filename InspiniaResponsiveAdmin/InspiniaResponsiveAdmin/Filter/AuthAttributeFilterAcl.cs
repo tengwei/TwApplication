bcd4deb2cb2919a488f3565e81af5ea274e209f5
tengwei <tengweichen@126.com>
2016-10-27 15:59:13 +0800

    1235

---
+++ InspiniaResponsiveAdmin/InspiniaResponsiveAdmin/Filter/AuthAttributeFilterAcl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InspiniaResponsiveAdmin.Filter {
+
+     [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class AuthAttributeFilterAcl : ActionFilterAttribute {
+        //不能录也可以有的权限
+         private static readonly List<string> Powerlist = new List<string> { 
+                "Account/Login".ToLower()
+            };
+        public override void OnActionExecuting (ActionExecutingContext filterContext) {
+           
+            string action = filterContext.RouteData.Values["action"].ToString().ToLower();
+            string controller = filterContext.RouteData.Values["controller"].ToString().ToLower();
+            //pass 登录相关
+            if (Powerlist.Any(s => s == (controller + "/" + action))) {
+                return;
+            }
+            if (!filterContext.HttpContext.Request.IsAuthenticated) {
+                filterContext.Result = new RedirectResult("/Account/Login"); 
+            }
+        }
+    }
+}
---
+++ InspiniaResponsiveAdmin/InspiniaResponsiveAdmin/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace InspiniaResponsiveAdmin.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "用户名")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+    }
+}
